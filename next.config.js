d3482848456a2ad7cd8b65aa5f849154c9976e5e
rogerSuperBuilderAlpha <roger.hunt@superbuilders.school>
2025-09-16 10:18:48 -0400

    Add webpack configuration to exclude functions directory from compilation and update tsconfig to reflect this exclusion

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  webpack: (config, { isServer }) => {
+    // Exclude functions directory from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/functions/**', '**/node_modules/**'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
